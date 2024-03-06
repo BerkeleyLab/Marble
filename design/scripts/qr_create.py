fa3bf2905ae6017f6580961a01d9f07b22358af2
Larry Doolittle <LRDoolittle@lbl.gov>
2024-03-05 22:00:51 -0800

    Change version marked on layout to v1.4.2
    Text in 4 places, plus the QR code
    Update version and instructions in qr_create.py

--- design/scripts/qr_create.py
+++ design/scripts/qr_create.py
@@ -1,20 +1,18 @@
 # In Debian Buster, apt-get install python-qrtools and run this program with python2.
-# In Debian Bullseye, apt-get install python3-qrtools and run this program with python3 (untested).
+# In Debian Bullseye, apt-get install python3-qrtools and run this program with python3.
 import qrtools
-ss = "https://github.com/BerkeleyLab/Marble/releases/tag/v1.4.1"
+ss = "https://github.com/BerkeleyLab/Marble/releases/tag/v1.4.2"
 qr = qrtools.QR(ss, pixel_size=8, margin_size=3, level='L')
 qr.encode("mm_qr")  # sends 312 x 312 pixel result to mm_qr.png
 
-# Next, use the "Bitmap to Component Converter" in the main KiCad project menu.
+# Next, use the "Image Converter" in the main KiCad project menu.
 # Load Bitmap and select mm_qr.png.
-# Choose 400 dpi to get a 19.8 mm output size,
-# and check "Negative" and "Front silk screen".
-# Export to logos/QR.kicad_mod
+# Choose 19.8 mm square output size,
+# and check "Footprint", "Negative", and "Front silk screen".
+# Export to File and select logos/QR.kicad_mod
 # In Pcbnew, select the QR code on the top silkscreen layer
-# and choose `Update Footprint ...` from the context menu
+# and choose `Update Footprint ...` from the context menu.
+# You should probably use your phone (or such) to confirm
+# that the resulting on-screen image reads as desired.
 
-# Above instructions are tested with KiCad 5.1.8
-# Be aware of KiCad issue #6514
-# https://gitlab.com/kicad/code/kicad/-/issues/6514
-# so if the results look corrupted, under KiCad Preferences
-# select "Modern Toolset (Fallback)".
+# Above instructions are tested with KiCad 6.0.11
